beae1aa9663403d412022cb383644db97a58f9ad
cs-sujeet-ahirrao <sahirrao@fortinet.com>
2023-04-20 19:20:19 +0530

    Added Hyperlink
    Added Hyperlink and copyright

--- taxii2-threat-intel-feed/connector.py
+++ taxii2-threat-intel-feed/connector.py
@@ -1,3 +1,10 @@
+"""
+Copyright start
+Copyright (C) 2008 - 2023 Fortinet Inc.
+All rights reserved.
+FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
+Copyright end
+"""
 from connectors.core.connector import Connector, get_logger, ConnectorError
 
 from .operations import operations, _check_health
