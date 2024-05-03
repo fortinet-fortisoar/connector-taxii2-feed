e5206e0d852caff7a98cd29a38afbf15515e8c63
randhale <randhale@fortinet.com>
2024-05-03 17:05:45 +0530

    Changes as per #1015928

--- taxii2-threat-intel-feed/connector.py
+++ taxii2-threat-intel-feed/connector.py
@@ -1,10 +1,9 @@
 """
 Copyright start
-Copyright (C) 2008 - 2023 Fortinet Inc.
-All rights reserved.
-FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
-Copyright end
+MIT License
+Copyright (c) 2024 Fortinet Inc Copyright end
 """
+
 from connectors.core.connector import Connector, get_logger, ConnectorError
 
 from .operations import operations, _check_health
